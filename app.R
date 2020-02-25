531e51bf2b7e8b15a4394470e7a544416c8b4895
Aep Hidayatuloh <aef.stk@gmail.com>
2020-02-25 17:15:17 +0000

    Update "gameplay"

--- app.R
+++ app.R
@@ -16,42 +16,41 @@ ui <- fluidPage(
            wellPanel(style="background-color:skyblue;border:none;color:white;",
                      h3("Usage", style="font-weight:bold;"),
                      p(style = "color:white;text-align:justify;",
-                       HTML('specify the parameter values (mu, sigma) and the lower and upper limits.
+                       HTML('specify the parameter values (\\(\\mu, \\sigma\\)), press the setup (gears icon) button and the lower and upper limits.
                        $$P(L \\lt X \\lt U) = \\int_{L}^{U}\\frac{1}{\\sigma\\sqrt{2\\pi}}e^{-\\frac{(x - \\mu)^2}{2\\sigma^2}}dx$$
                        <br/>
-                       $$ = \\int_{-\\infty}^{U}\\frac{1}{\\sigma\\sqrt{2\\pi}}e^{-\\frac{(x - \\mu)^2}{2\\sigma^2}}dx - \\\\ \\int_{-\\infty}^{L}\\frac{1}{\\sigma\\sqrt{2\\pi}}e^{-\\frac{(x - \\mu)^2}{2\\sigma^2}}dx$$
                             <br/>
                             If you want to find the z-value for a significant value \\(\\alpha\\) (alpha) then enter the alpha value on the <strong>"Calculate Z-value Table"</strong> section. 
-                            The Z-value will be calculated for \\(\\frac{\\alpha}{2}\\). \\(Z =  1 - P(X \\lt x)\\).'))
+                            The Z-value will be calculated for \\(\\frac{\\alpha}{2}\\). \\(1 - P(X \\lt x)\\).'))
                      )
            ),
     column(width = 3,
-           h4("Normal Distribution Parameter"),
-           div(style="display:inline-block;vertical-align:top;width:49%;font-size:180%;", numericInput("mu", expression("\u03BC"), value = 0)),
-           div(style="display:inline-block;vertical-align:top;width:49%;font-size:180%;", numericInput("sigma", expression("\u03C3"), value = 1, min = 0.1, step = 0.1)),
+           h5("Normal Distribution Parameter"),
+           div(style="display:inline-block;vertical-align:top;width:42%;font-size:120%;", numericInput("mu", expression("\u03BC"), value = 0)),
+           div(style="display:inline-block;vertical-align:top;width:42%;font-size:120%;", numericInput("sigma", expression("\u03C3"), value = 1, min = 0.1, step = 0.1)),
+           div(style="display:inline-block;vertical-align:top;width:10%;margin-top:28px;", actionButton("updParam", NULL, icon = icon("cogs"))),
            hr(),
            helpText("Cumulative value between lower and upper limits"),
-           # div(style="display:inline-block;vertical-align:top;width:49%;", numericInput("nilai1", "Lower", value = -1.96, step = 0.1)),
-           # # div(style="display:inline-block;vertical-align:top;width:20%;", h2(" < X < ")),
-           # div(style="display:inline-block;vertical-align:top;width:49%;", numericInput("nilai2", "Upper", value = 1.96, step = 0.1)),
-           # p("$$P(L \\lt X \\lt U) = \\int_{L}^{U}\\frac{1}{\\sigma\\sqrt{2\\pi}}e^{-\\frac{(x - \\mu)^2}{2\\sigma^2}}dx$$"),
            conditionalPanel(condition = "input.nilai2 < input.nilai1",
                             p("The Upper Limit value cannot be smaller than the Lower Limit value.", style = "color:red;font-size:90%;")
            ),
            uiOutput("prob"),
            hr(),
-           h4("Calculate Z-value Table"),
-           div(style="display:inline-block;vertical-align:top;width:29%;font-size:180%;", numericInput("alpha", expression("\u03B1"), value = 0.05, step = 0.001, min = 0.00001, max = 0.99999)),
-           div(style="display:inline-block;vertical-align:top;width:69%;font-size:180%;", numericInput("zv", paste0("Z-value (", expression("\u03B1"), "/2)"), value = 1.96)),
-           hr()
-           
+           h5("Calculate Z-value Table"),
+           div(style="display:inline-block;vertical-align:top;width:32%;font-size:120%;", numericInput("alpha", expression("\u03B1"), value = 0.05, step = 0.001, min = 0.00001, max = 0.99999)),
+           div(style="display:inline-block;vertical-align:top;width:52%;font-size:120%;", numericInput("zv", paste0("Z-value (", expression("\u03B1"), "/2)"), value = 1.96)),
+           div(style="display:inline-block;vertical-align:top;width:10%;margin-top:28px;", actionButton("calcZv", NULL, icon = icon("calculator"))),
+           hr(),
+           h5("Standardized Value"),
+           div(style="display:inline-block;vertical-align:top;width:32%;font-size:120%;", numericInput("xval", "X", value = 0)),
+           div(style="display:inline-block;vertical-align:top;width:52%;font-size:120%;", textInput("std", paste0("Z = (X - ", expression("\u03BC"), ")/", expression("\u03C3")), value = 0)),
+           div(style="display:inline-block;vertical-align:top;width:10%;margin-top:28px;", actionButton("calcStd", NULL, icon = icon("calculator")))
     ),
     column(width = 6,
            # uiOutput("slider"),
            sliderInput("sliderrange", label = h4("Lower and Upper limits"), min = -4.5, max = 4.5, value = c(-1.96, 1.96), step = 0.01, width = "100%"),
            uiOutput("plotTitle"),
            plotOutput("plotPeluang")
-           # plotlyOutput("plotPeluang")
     )
   )
                           
@@ -59,33 +58,48 @@ ui <- fluidPage(
 
 server <- function(input, output, session){
   # Menu Menghitung Peluang Normal
-  output$plotTitle <- renderUI({
-    h4(style = "text-align:center;", paste0("Normal Distribution with ", expression("\u03BC = "), input$mu, expression(" dan \u03C3 = "), input$sigma))
+  
+  observe({
+    disable("zv")
+    disable("std")    
   })
   
-  observeEvent(input$alpha,{
+  param <- eventReactive(input$updParam, {
+    mu = as.numeric(input$mu)
+    sigma = as.numeric(input$sigma)
+    list(mu = mu, sigma = sigma)
+  })
+  
+  output$plotTitle <- renderUI({
+    param <- param()
+    h4(style = "text-align:center;", paste0("Normal Distribution with ", expression("\u03BC = "), param$mu, expression(" dan \u03C3 = "), param$sigma))
+  })
+  
+  observeEvent(input$calcZv,{
     req(input$alpha)
-    disable("zv")
+    isolate(input$alpha)
     updateNumericInput(session, "zv", paste0("Z-value (", expression("\u03B1"), "/2)"), value = round(qnorm(1 - as.numeric(input$alpha)/2), 2))
   })
   
-  ip <- reactive({
-    req(input$mu)
-    req(input$sigma)
-    mu = as.numeric(input$mu)
-    sigma = as.numeric(input$sigma)
-    mu*sigma
-  })
-
-  observeEvent(ip(), {
-    mu = as.numeric(input$mu)
-    sigma = as.numeric(input$sigma)
-    
+ observeEvent(input$calcStd, {
+   req(input$xval)
+   req(input$mu)
+   req(input$sigma)
+   isolate(input$xval)
+   # param <- param()
+   z <- (as.numeric(input$xval) - input$mu)/input$sigma
+   updateTextInput(session, "std", paste0("Z = (X - ", expression("\u03BC"), ")/", expression("\u03C3")), value = round(z, 2))
+ })
+  
+  observeEvent(input$updParam, {
+    param <- param()
+    mu <- param$mu
+    sigma <- param$sigma
     # a = as.numeric(input$sliderrange[1])
     # b = as.numeric(input$sliderrange[2])
-    if(input$mu == "" | input$sigma == ""
-       | input$mu == "-" | input$sigma == "-"
-       | is.null(input$mu) | is.null(input$sigma))
+    if(mu == "" | sigma == ""
+       | mu == "-" | sigma == "-"
+       | is.null(mu) | is.null(sigma))
     {
       return(NULL)
     }
@@ -93,20 +107,21 @@ server <- function(input, output, session){
     bawah.x = mu - 4.5*sigma
     atas.x = mu + 4.5*sigma
     # output$slider <- renderUI({
-      updateSliderInput(session, "sliderrange", label = "Lower and Upper limits", min = bawah.x, max = atas.x, value = c(-1.96, 1.96), step = 0.01)
+      updateSliderInput(session, "sliderrange", label = "Lower and Upper limits", min = bawah.x, max = atas.x, step = 0.01)
     # })
   })
 
  
   probs <- reactive({
-    mu = as.numeric(input$mu)
-    sigma = as.numeric(input$sigma)
+    param <- param()
+    mu  <- as.numeric(param$mu)
+    sigma  <- as.numeric(param$sigma)
     
     a = as.numeric(input$sliderrange[1])
     b = as.numeric(input$sliderrange[2])
-    if(input$mu == "" | input$sigma == ""
-       | input$mu == "-" | input$sigma == "-"
-       | is.null(input$mu) | is.null(input$sigma))
+    if(mu == "" | sigma == ""
+       | mu == "-" | sigma == "-"
+       | is.null(mu) | is.null(sigma))
     {
       return(NULL)
     }
@@ -197,141 +212,75 @@ server <- function(input, output, session){
     p(sprintf("P(%s < X < %s) = %s%%", input$sliderrange[1], input$sliderrange[2], round((probs$probtot1 + probs$probtot2)*100, 1)), style="text-align:center;font-weight:bold;color:black;font-size:150%;")
   })
   
-  
-  output$plotPeluang <- renderPlot({
-    mu = as.numeric(input$mu)
-    sigma = as.numeric(input$sigma)
-    
-    a = as.numeric(input$sliderrange[1])
-    b = as.numeric(input$sliderrange[2])
-    if(input$mu == "" | input$sigma == ""
-       | input$mu == "-" | input$sigma == "-"
-       | is.null(input$mu) | is.null(input$sigma))
-    {
-      return(NULL)
-    }
-    
-    bawah.x = mu - 4.5*sigma
-    atas.x = mu + 4.5*sigma
-    # posisi = b + 0.7
-    # 
-    # peluang1 = pnorm(a, mu, sigma)
-    # if(b <= mu) {
-    #   peluang2 = pnorm(b, mu, sigma) - peluang1
-    # } else {
-    #   peluang2 = pnorm(mu, mu, sigma) - peluang1
-    # }
-    # if(a >= mu) {
-    #   peluang3 = pnorm(b, mu, sigma) - pnorm(a, mu, sigma)
-    # } else {
-    #   peluang3 = pnorm(b, mu, sigma) - pnorm(mu, mu, sigma)
-    # }
-    # peluang4 = 1 - pnorm(b, mu, sigma)
-    # 
-    # if(b <= bawah.x) {
-    #   bawah.x <- b
-    #   # posisi <- b * 1.1 
-    #   # peluang = 1 - peluang
-    # }
-    # if(a >= atas.x) {
-    #   atas.x <- a
-    #   # posisi <- a * 1.1
-    #   # peluang = 1 - peluang
-    # }
-    # 
-    probs <- probs()
-    
-    # df <- data.frame(poly.x = c(a, seq(a, b, 0.01), b),
-    #                  poly.y = c(0, dnorm(seq(a, b, 0.01), mu, sigma), 0)
-    # )
-    # df_norm <- data.frame(curve.x = c(bawah.x, seq(bawah.x, atas.x, 0.01), atas.x),
-    #                       curve.y = c(0, dnorm(seq(bawah.x, atas.x, 0.01), mu, sigma), 0)
-    # )
-    
-    p9 <- ggplot(data = probs$df_norm, aes(x = curve.x, y = curve.y)) +
-      geom_area(fill = "lightblue", colour = "skyblue", size = 1.2) +
-      geom_polygon(data = probs$df, aes(x = poly.x, y = poly.y), fill = "#71A9CA") +
-      geom_vline(xintercept = a, linetype = "dotted", colour = "#F24343") +
-      geom_vline(xintercept = b, linetype = "dotted", colour = "#F24343") +
-      geom_vline(xintercept = mu, linetype = "dotted", colour = "black") 
-    # geom_segment(aes(x = a, xend = a, y = 0, yend = df_norm$curve.y[round(df_norm$curve.x, 2) == round(a, 2)]), linetype = "dotted", colour = "#F24343") +
-    # geom_segment(aes(x = b, xend = b, y = 0, yend = df_norm$curve.y[round(df_norm$curve.x, 2) == round(b, 2)]), linetype = "dotted", colour = "#F24343") +
-    # anotasi lower.tail
-    if(probs$peluang1*100 >= 1) {
-      p9 <- p9 + annotate("text",
-                          label = paste0(round(probs$peluang1*100, 1), "%"),
-                          x = a - 0.4, #posisi,
-                          y = max(probs$df_norm$curve.y) * 1.2,
-                          size = 5, colour = "lightblue")
-    }
-    x2 <- ifelse(b < mu, b - 0.4, mu - 0.4)
-    # if(probs$peluang2*100 > 0) {
-    #   p9 <- p9 + annotate("text",
-    #                       label = paste0(round(probs$peluang2*100, 1), "%"),
-    #                       x = x2, #posisi,
-    #                       y = max(probs$df_norm$curve.y) * 1.1,
-    #                       size = 5, colour = "#71A9CA") 
-    # }
-    # anotasi upper.tail
-    x3 <- ifelse(a > mu, a + 0.4, mu + 0.4)
-    # if(probs$peluang3*100 > 0) {
-    #   p9 <- p9 + annotate("text",
-    #                       label = paste0(round(probs$peluang3*100, 1), "%"),
-    #                       x = x3, #posisi,
-    #                       y = max(probs$df_norm$curve.y) * 1.1,
-    #                       size = 5, colour = "#71A9CA")
-    # }
-    if(probs$peluang4*100 >= 1) {
-      p9 <- p9 + annotate("text",
-                          label = paste0(round(probs$peluang4*100, 1), "%"),
-                          x = b + 0.4, #posisi,
-                          y = max(probs$df_norm$curve.y) * 1.2,
-                          size = 5, colour = "lightblue")
-    }
-    
-    # annotate("text",
-    #          label = expression("\u03BC"),
-    #          x = mu,
-    #          y = 0.1,
-    #          size = 3, colour = "black") +
-    # if(probs$peluang2 >= 0 & probs$peluang3 >= 0) {
-    #   probtot <- peluang2 + peluang3
-    # } else if(peluang2 < 0 & peluang3 >= 0) {
-    #   probtot <- peluang3
-    # } else if(peluang2 >= 0 & peluang3 < 0) {
-    #   probtot <- peluang2
-    # } else {
-    #   probtot <- 0
-    # }
-    
-    p9 <- p9 +
-      xlim(probs$bawah.x, probs$atas.x) +
-      scale_y_continuous(expand = c(0, 0), limits = c(0, max(probs$df_norm$curve.y) * 1.3)) +
-      labs(title = paste0("P(", a, " < X < ", b, ") = ", round(probs$probtot1*100, 1), "% + ", round(probs$probtot2*100, 1), "%\n"),
-           x = "X") +
-      # stat_function(fun = funcShaded, args = list(mu, sigma, a, b), geom = "area", fill = "#84CA72", alpha = 0.2) +
-      theme_classic() +
-      theme(
-        plot.title = element_text(hjust = 0.5, color = "#71A9CA", size = 14, face = "bold"),
-        axis.line.y = element_blank(),
-        axis.title.y = element_blank(),
-        axis.text.y = element_blank(),
-        axis.ticks.y = element_blank(),
-        axis.title.x = element_text(size = 14, face = "bold"),
-        panel.background = element_blank(),
-        panel.grid.minor = element_blank(),
-        panel.grid.major = element_blank(),
-        plot.background = element_blank()
-      )
-    p9
-    # ggplotly(p9, tooltip = "text") %>%
-    # plot_ly(xnorm, x = ~x, y = ~y,
-    #         type = 'scatter', mode = 'lines', fill = 'tozeroy') %>%
-    # layout(#title = paste0("Sebaran Normal dengan $mu$ = ", mu, ", $sigma$ = ", sigma),
-    # paper_bgcolor = "rgba(0, 0, 0, 0)",
-    # plot_bgcolor = "rgba(0, 0, 0, 0)")
-    
-  }, height = 350)
+  # observeEvent(input$sliderrange, {
+    output$plotPeluang <- renderPlot({
+      param <- param()
+      mu  <- as.numeric(param$mu)
+      sigma <- as.numeric(param$sigma)
+      
+      a = as.numeric(input$sliderrange[1])
+      b = as.numeric(input$sliderrange[2])
+      if(mu == "" | sigma == ""
+         | mu == "-" | sigma == "-"
+         | is.null(mu) | is.null(sigma))
+      {
+        return(NULL)
+      }
+      
+      bawah.x = mu - 4.5*sigma
+      atas.x = mu + 4.5*sigma
+      # posisi = b + 0.7
+      # 
+
+      probs <- probs()
+      
+      p9 <- ggplot(data = probs$df_norm, aes(x = curve.x, y = curve.y)) +
+        geom_area(fill = "lightblue", colour = "skyblue", size = 1.2) +
+        geom_polygon(data = probs$df, aes(x = poly.x, y = poly.y), fill = "#71A9CA") +
+        geom_vline(xintercept = a, linetype = "dotted", colour = "#F24343") +
+        geom_vline(xintercept = b, linetype = "dotted", colour = "#F24343") +
+        geom_vline(xintercept = mu, linetype = "dotted", colour = "black") 
+      if(probs$peluang1*100 >= 1) {
+        p9 <- p9 + annotate("text",
+                            label = paste0(round(probs$peluang1*100, 1), "%"),
+                            x = a - 0.4, #posisi,
+                            y = max(probs$df_norm$curve.y) * 1.2,
+                            size = 5, colour = "lightblue")
+      }
+      x2 <- ifelse(b < mu, b - 0.4, mu - 0.4)
+
+      # anotasi upper.tail
+      x3 <- ifelse(a > mu, a + 0.4, mu + 0.4)
+      if(probs$peluang4*100 >= 1) {
+        p9 <- p9 + annotate("text",
+                            label = paste0(round(probs$peluang4*100, 1), "%"),
+                            x = b + 0.4, #posisi,
+                            y = max(probs$df_norm$curve.y) * 1.2,
+                            size = 5, colour = "lightblue")
+      }
+      
+      p9 <- p9 +
+        xlim(probs$bawah.x, probs$atas.x) +
+        scale_y_continuous(expand = c(0, 0), limits = c(0, max(probs$df_norm$curve.y) * 1.3)) +
+        labs(title = paste0("P(", a, " < X < ", b, ") = ", round(probs$probtot1*100, 1), "% + ", round(probs$probtot2*100, 1), "%\n"),
+             x = "X") +
+        theme_classic() +
+        theme(
+          plot.title = element_text(hjust = 0.5, color = "#71A9CA", size = 14, face = "bold"),
+          axis.line.y = element_blank(),
+          axis.title.y = element_blank(),
+          axis.text.y = element_blank(),
+          axis.ticks.y = element_blank(),
+          axis.title.x = element_text(size = 14, face = "bold"),
+          panel.background = element_blank(),
+          panel.grid.minor = element_blank(),
+          panel.grid.major = element_blank(),
+          plot.background = element_blank()
+        )
+      p9
+    }, height = 350)
+  # })
+
 }
 
 shinyApp(ui, server)
